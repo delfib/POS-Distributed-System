a4b50bfc2bc0911538cae6ebba8adc6d1f58199f
eabalestra <agusbalestra0@gmail.com>
2025-12-06 18:09:32 -0300

    Delete unnussed classes

--- src/display/network_display.py
+++
@@ -1,9 +0,0 @@
-class Network:
-    def __init__(self):
-        self.nodes = []
-
-    def add_node(self, node):
-        self.nodes.append(node)
-
-    def remove_node(self, node):
-        self.nodes.remove(node)
