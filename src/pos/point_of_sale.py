80fc99712d6c2725fc11741298ad009534856ee8
eabalestra <agusbalestra0@gmail.com>
2025-12-07 18:02:50 -0300

    Refactor Point of Sale system: enhance initialization with database path, implement peer communication in buy method, and update server routes
    Co-authored-by: José Ignacio Compagnucci Bruno <JoseCompag@users.noreply.github.com>
    Co-authored-by: Delfina Buil <delfib@users.noreply.github.com>

--- src/pos/point_of_sale.py
+++ src/pos/point_of_sale.py
@@ -1,8 +1,35 @@
 from __future__ import annotations
 
+from src.pos.deposit import Deposit
+from src.pos.transactions import ProductStockManager
+
 
 class PointOfSale:
-    def __init__(self, node_id: str, host: str, port: int):
+    def __init__(self, node_id: str, host: str, port: int, db_path: str) -> None:
         self.node_id = node_id
         self.host = host
         self.port = port
+        # Peers in the distributed system
+        self.peers: dict[str, PointOfSale] = {
+            "pos-2": PointOfSale("pos-2", "0.0.0.0", 8001, db_path="src/db/db2.json"),
+            "pos-3": PointOfSale("pos-3", "0.0.0.0", 8002, db_path="src/db/db3.json"),
+        }
+        self.deposit = Deposit("src/db/db1.json")
+        self.product_manager = ProductStockManager(node_id, self.deposit)
+
+    def start(self) -> None:
+        print(f"Point of Sale {self.node_id} started at {self.host}:{self.port}")
+
+    def buy(self, product_id: str, quantity: int) -> bool:
+        is_in_my_stock = self.product_manager.sell_product(product_id, quantity)
+        if is_in_my_stock:
+            return True
+        # Ask peers
+        for peer_id, peer in self.peers.items():
+            # establecer conexión con el peer
+            # enviar solicitud de compra
+            # esperar respuesta
+            # if peer_can_sell:
+            #     return True
+            pass
+        return False
--- src/server.py
+++ src/server.py
@@ -11,20 +11,22 @@ class Server:
         pos = PointOfSale(node_id=node_id, host=host, port=port)
         app = FastAPI(title="Distributed POS", version="0.1.0")
 
+        @app.on_event("startup")
+        async def startup_event():
+            pos.start()
+
         @app.post("/buy/{product_id}")
-        async def buy(product_id: str):
-            return
+        async def buy(product_id: str, quantity: int = 1):
+            pos.buy(product_id, quantity)
+            return {"status": "success", "product_id": product_id, "quantity": quantity}
 
         @app.get("/product/{product_id}")
         async def get_product(product_id: str):
             return
 
+        # solo para nodos lideres
         @app.put("/product/{product_id}/price")
         async def update_price(product_id: str):
-            return
-
-        @app.post("/raft/x")
-        async def raft():
             return
 
         return app
