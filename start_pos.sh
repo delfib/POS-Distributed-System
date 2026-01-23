a45173a7e6c77657a032128a651cf98e9118490f
eabalestra <agusbalestra0@gmail.com>
2026-01-23 18:38:48 -0300

    Add start_pos.sh script to launch multiple POS instances in separate terminals
    Co-authored-by: José Ignacio Compagnucci Bruno <JoseCompag@users.noreply.github.com>
    Co-authored-by: Delfina Buil <delfib@users.noreply.github.com>

---
+++ start_pos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gnome-terminal -- bash -c "source .venv/bin/activate && python3 src/server.py --id POS1; exec bash"
+gnome-terminal -- bash -c "source .venv/bin/activate && python3 src/server.py --id POS2; exec bash"
+gnome-terminal -- bash -c "source .venv/bin/activate && python3 src/server.py --id POS3; exec bash"
